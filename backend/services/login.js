169664845b99629e4648a15a62d829a0f4a60ebb
Rodrigo Rivera <rodriggo5r17@gmail.com>
2025-05-09 21:19:42 -0300

    Creación de service y middleware.

---
+++ backend/services/login.js
@@ -0,0 +1,24 @@
+export async function loginService(credentials){
+    if (!credentials || !credentials.username || !credentials.password || typeof credentials.username !== 'string' || typeof credentials.password !== 'string'){
+        return {
+            error: 'Argumentos no válidos.'
+        };
+        //throw new InvalidArgumentException();
+    }
+
+    if (credentials.username !== 'admin'){
+        return {
+            error: 'Credenciales inválidas.',
+        };
+    }
+    
+    if (credentials.password !== '1234'){
+        return {
+            error: 'Credenciales inválidas.'
+        };
+    }
+
+    return {
+        token: 'Token de acceso'
+    };
+}
