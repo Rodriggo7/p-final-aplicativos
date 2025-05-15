e212287a2cb3cefac5dbea505e25072ad9f25bbb
Rodrigo Rivera <rodriggo5r17@gmail.com>
2025-05-14 22:09:39 -0300

    adición middlewares: error_handler_middleware.js, log.middleware.js

--- backend/middlewares/log.middleware.js
+++ backend/middlewares/log.middleware.js
@@ -1,5 +1,19 @@
 export function logMiddleware(req, res, next) {
-    const date = (new Date().toISOString()).replace(/T/, ' ');
-    console.log(`[${date}] ${req.ip} ${req.method} ${req.url}`);
+    const date = new Date()
+    .toISOString()
+    .replace(/T/, ' ');
+    const ip = req.ip;
+    const request = req.method + ' ' + req.url;
+    
+    const body = {...req.body };
+    if (body.password) {
+        body.password = '********';
+    }
+
+
+    const bodyText = JSON.stringify(body);
+
+    console.log(`[${date}] ${ip} ${request} ${bodyText}`);
+    
     next();
 }
