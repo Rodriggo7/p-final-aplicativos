d1f4e36dde7494afd9bf57379da5c202671216ba
Rodrigo Rivera <rodriggo5r17@gmail.com>
2025-05-14 22:29:13 -0300

    Modificación services login.js

--- backend/services/login.js
+++ backend/services/login.js
@@ -1,24 +1,24 @@
-export async function loginService(credentials){
-    if (!credentials || !credentials.username || !credentials.password || typeof credentials.username !== 'string' || typeof credentials.password !== 'string'){
-        return {
-            error: 'Argumentos no válidos.'
-        };
-        //throw new InvalidArgumentException();
-    }
+import { InvalidArgumentException } from "../exceptions/invalid_argument_exception.js";
+import { InvalidCredentialsException } from "../exceptions/invalid_credentials_exception.js";
 
-    if (credentials.username !== 'admin'){
-        return {
-            error: 'Credenciales inválidas.',
-        };
-    }
+export class LoginService {
+    static async login(credentials){
+    if (!credentials 
+        || !credentials.username 
+        || !credentials.password 
+        || typeof credentials.username !== 'string' 
+        || typeof credentials.password !== 'string'
+        )
+        throw new InvalidArgumentException();
+
+    if (credentials.username !== 'admin')
+        throw new InvalidCredentialsException();
     
-    if (credentials.password !== '1234'){
-        return {
-            error: 'Credenciales inválidas.'
-        };
-    }
+    if (credentials.password !== '1234')
+        throw new InvalidCredentialsException();
 
     return {
         token: 'Token de acceso'
     };
+    }
 }
